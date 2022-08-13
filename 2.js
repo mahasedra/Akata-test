045559c72f8be768e33e7210ecd378313e4f30ec
Mahasedra <mahasedra01@gmail.com>
2022-08-13 15:29:58 +0300

    test akata response 2

--- 2.js
+++ 2.js
@@ -2,7 +2,7 @@ function nonConsecutiveDigits(N) {
     const J = N + 1;
     stringNumber = J.toString();
     for (i=1; i< stringNumber.length; i++){
-        if (stringNumber.charAt(index) === stringNumber.charAt(index - 1)) nonConsecutiveDigits(J);
+        if (stringNumber.charAt(index) === stringNumber.charAt(index - 1)) J =nonConsecutiveDigits(J);
     };
     return J;
 }
